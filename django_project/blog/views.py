739063f35de2a1d0e6bfa93a928c65b5e06e0e6a
Alexi Jordanov <alexi.j@samsung.com>
2024-07-08 22:19:14 -0700

    Learn Django

---
+++ django_project/blog/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'AlexiworlD',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27th, 2018'
+    },
+    {
+        'author': 'Trump vs Biden',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28th, 2018'
+    }
+]
+
+# Create your views here.
+def home(request):
+    # return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context) # the path under templates directory
+
+def about(request):
+    #return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
