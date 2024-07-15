74cd3c33df2f68d66700a2497e156d9e8b3f05f1
Alexi Jordanov <alexi.j@samsung.com>
2024-07-15 14:00:43 -0700

    Posts ORM, registering with Admin console to CRUD posts

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -9,3 +9,12 @@ class Post(models.Model):
     content = models.TextField() # unrestricted in length text
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE) # what to do with the post if the user is deleted
+
+    # after creating this file run
+    #   python3 manage.py makemigrations
+    #   python3 manage.py sqlmigrate blog 0001
+    # make sure 0001 is the number received in the first step
+    #   python3 manage.py migrate
+
+    def __str__(self) -> str:
+        return self.title
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,26 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
-posts = [
-    {
-        'author': 'AlexiworlD',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27th, 2018'
-    },
-    {
-        'author': 'Trump vs Biden',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28th, 2018'
-    }
-]
+# posts = [
+#     {
+#         'author': 'AlexiworlD',
+#         'title': 'Blog Post 1',
+#         'content': 'First post content',
+#         'date_posted': 'August 27th, 2018'
+#     },
+#     {
+#         'author': 'Trump vs Biden',
+#         'title': 'Blog Post 2',
+#         'content': 'Second post content',
+#         'date_posted': 'August 28th, 2018'
+#     }
+# ]
 
 # Create your views here.
 def home(request):
     # return HttpResponse('<h1>Blog Home</h1>')
     context = {
-        'posts': posts
+        'posts': Post.objects.all() # posts
     }
     return render(request, 'blog/home.html', context) # the path under templates directory
 
