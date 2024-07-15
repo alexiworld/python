bd0ccaac320d5cab7cb6da047f1c8c2299a75fba
Alexi Jordanov <alexi.j@samsung.com>
2024-07-15 14:32:01 -0700

    Adding redirect to home page upon success, fixing missing messages import. Note that qwerty is frequently used password and therefore registration with this password will fail.

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 # Create your views here.
 def register(request):
@@ -8,6 +9,7 @@ def register(request):
         if form.is_valid():
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}!')
+            return redirect('blog-home')
     else:
         form = UserCreationForm()
     return render(request, 'users/register.html', {'form': form})
