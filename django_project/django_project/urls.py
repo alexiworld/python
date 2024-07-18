c4b1c6e6177e56b1114a5579f65d74c545e8ac81
Alexi Jordanov <alexi.j@samsung.com>
2024-07-18 14:37:10 -0700

    Add profile snippet and reconfigure image location and rendering

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -19,6 +19,10 @@ from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from users import views as user_views
 
+# https://docs.djangoproject.com/en/2.1/howto/static-files/#serving-files-uploaded-by-a-user-during-development
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register', user_views.register, name='register'),
@@ -27,4 +31,7 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('blog/', include('blog.urls')),
     path('', include('blog.urls')),
-]
+] 
+
+if settings.DEBUG: 
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
