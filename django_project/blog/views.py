e1e1bbb4464ab9a0dac40a76c7bf63a3bbdd51cd
Alexi Jordanov <alexi.j@samsung.com>
2024-07-25 10:21:41 -0700

    Switch to using ListView, and reorder posts newest on the top

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView
 from .models import Post
 
 # posts = [
@@ -24,6 +25,15 @@ def home(request):
         'posts': Post.objects.all() # posts
     }
     return render(request, 'blog/home.html', context) # the path under templates directory
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html' # overriding default <app>/<model>_<viewtype>.html
+    # overriding ListView's default name used in context to 'posts', 
+    # i.e. to match the one used in home function (see context variable with 'posts' above).
+    context_object_name = 'posts' 
+    ordering = ['-date_posted']
+
 
 def about(request):
     #return HttpResponse('<h1>Blog About</h1>')
