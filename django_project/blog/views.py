3f0cb08ae9ee7ca0a7ff67eed3f721f5113b0a55
Alexi Jordanov <alexi.j@samsung.com>
2024-07-25 11:32:54 -0700

    Protect /post/new by redirecting anonymous users to Login page

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,4 +1,5 @@
 from django.forms import BaseModelForm
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import ListView, DetailView, CreateView
@@ -38,7 +39,7 @@ class PostListView(ListView):
 class PostDetailView(DetailView):
     model = Post
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     fields = ['title', 'content']
 
