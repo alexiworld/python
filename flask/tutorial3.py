4326128f4c56d8e714340e49f16c2f0a162fca3e
Alexi Jordanov <alexiworld@yahoo.com>
2024-07-29 16:39:39 -0700

    Forms with Flask

--- flask/tutorial3.py
+++ flask/tutorial3.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html", content="Testing")
+    return render_template("tutorial3/index.html")
+
+@app.route("/about")
+def about():
+    return render_template("tutorial3/about.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
