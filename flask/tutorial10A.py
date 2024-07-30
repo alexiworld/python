fd47868fa0a11a5c284d4f022d2c012b88612e14
Alexi Jordanov <alexiworld@yahoo.com>
2024-07-30 11:34:15 -0700

    Fix misspelled url_prefix

--- flask/tutorial10A.py
+++ flask/tutorial10A.py
@@ -2,7 +2,9 @@ from flask import Flask, render_template
 from tutorial10B import blueprint
 
 app = Flask(__name__)
-app.register_blueprint(blueprint, utl_prefix="")
+app.register_blueprint(blueprint, url_prefix="") # "/bp"
+# To learn more https://flask.palletsprojects.com/en/3.0.x/blueprints/
+print(app.url_map)
 
 @app.route('/')
 def home():
