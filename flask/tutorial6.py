7960acb3d44143a1bf7c5ef265726608afe4bce3
Alexi Jordanov <alexiworld@yahoo.com>
2024-07-29 17:34:45 -0700

    Add Login/Logout links and make them work

---
+++ flask/tutorial6.py
@@ -0,0 +1,39 @@
+from flask import Flask, redirect, url_for, render_template, request, session, flash
+from datetime import timedelta
+
+app = Flask(__name__)
+app.secret_key = "FlaskTutorialSecretKey"
+app.permanent_session_lifetime = timedelta(minutes=30) #days=5
+
+@app.route("/")
+def home():
+    return render_template("tutorial5/index.html")
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        user = request.form["nm"]
+        session.permanent = True # required for permanent_session_lifetime to work
+        session["user"] = user
+        return redirect(url_for("user"))
+    else:
+        if "user" in session:
+            return redirect(url_for("user")) 
+        return render_template("tutorial6/login.html")
+
+@app.route("/user")
+def user():
+    if "user" in session:
+        user = session["user"]
+        return render_template("tutorial6/user.html", content=["time", "money", "soul"], user=user)
+    else:
+        return redirect(url_for("login"))
+    
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    flash("You have been successfully logged out!", "info")
+    return redirect(url_for("login"))
+
+if __name__ == "__main__":
+    app.run(debug=True)
