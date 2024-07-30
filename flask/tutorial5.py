5cc3663101ed1c72002a82328990f90a32dc3402
Alexi Jordanov <alexiworld@yahoo.com>
2024-07-29 17:06:08 -0700

    Make session permanent for minutes or days

--- flask/tutorial5.py
+++ flask/tutorial5.py
@@ -1,7 +1,9 @@
 from flask import Flask, redirect, url_for, render_template, request, session
+from datetime import timedelta
 
 app = Flask(__name__)
 app.secret_key = "FlaskTutorialSecretKey"
+app.permanent_session_lifetime = timedelta(minutes=30) #days=5
 
 @app.route("/")
 def home():
@@ -11,6 +13,7 @@ def home():
 def login():
     if request.method == "POST":
         user = request.form["nm"]
+        session.permanent = True # required for permanent_session_lifetime to work
         session["user"] = user
         return redirect(url_for("user"))
     else:
