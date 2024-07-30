f99b12bfd3a4829655a82fe838269f68e195ec53
Alexi Jordanov <alexiworld@yahoo.com>
2024-07-30 11:37:05 -0700

    Move blueprint to appear under /bp root, leaving the main app under / root

--- flask/tutorial10B.py
+++ flask/tutorial10B.py
@@ -6,3 +6,7 @@ blueprint = Blueprint("blueprint", __name__, static_folder='static', template_fo
 @blueprint.route('/')
 def home():
     return render_template("tutorial10/home.html")
+
+@blueprint.route('/test')
+def test():
+    return "<h1>BP Test</h1>";
